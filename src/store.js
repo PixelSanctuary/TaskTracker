25ee22f5d372f29a39bf0f50ac632e108cc42911
Pixel Sanctuary <sanctuarypixel@gmail.com>
2024-02-27 15:18:17 +0530

    bug fixes

--- src/store.js
+++ src/store.js
@@ -14,6 +14,12 @@ const state = reactive({
     description: "Hit the GYM burn calories and get into shape, take good protiew for muccle growth",
     isCompleted: false,
     priorityIndex: 1
+  },
+  {  
+    title: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eleifend nec libero at pretium. Suspendisse potenti. Sed congue bibendum bibendum. Morbi ornare dolor quis magna semper hendrerit. Duis malesuada bibendum lacus eu efficitur. Nulla ac viverra elit. Aenean maximus elit condimentum dolor sollicitudin malesuada. Nam et ornare metus. Mauris sit amet accumsan justo. Curabitur vel vulputate neque. Praesent vehicula leo nulla, eget ornare massa ultricies eu. Sed diam purus, lacinia facilisis ornare vitae, feugiat nec sem. Sed velit justo, luctus ac facilisis vel, elementum sed quam. Nulla at ullamcorper ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis euismod eleifend eros, et interdum lectus ornare vitae.",
+    isCompleted: false,
+    priorityIndex: 3
   }
 ],
   selectedTask: null,
